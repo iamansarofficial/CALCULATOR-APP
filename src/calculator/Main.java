58ff8c9cbe4f7220c1a98f59a3e2aef4064704cc
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-01-13 12:27:10 +0530

    ALL BUTTONS ARE CREATE

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -21,8 +21,46 @@ public class Main implements ActionListener {
 		frame.setLayout(null);
 		
 		textfield=new JTextField();
+		textfield.setFont(font);
 		textfield.setBounds(50,25,300,50);
 		textfield.setBackground(Color.white);
+		textfield.setEditable(false);
+		
+		addButton=new JButton("+");
+		subButton=new JButton("-");
+		mulButton=new JButton("*");
+		divButton=new JButton("/");
+		decButton=new JButton(".");
+		clrButton=new JButton("Clr");
+		delButton=new JButton("Del");
+		equButton=new JButton("=");
+		negButton=new JButton("(-)");
+		
+		functionsButton[0] = addButton;
+		functionsButton[1] = subButton;
+		functionsButton[2] = mulButton;
+		functionsButton[3] = divButton;
+		functionsButton[4] = decButton;
+		functionsButton[5] = equButton;
+		functionsButton[6] = delButton;
+		functionsButton[7] = clrButton;
+		functionsButton[8] = negButton;
+		
+		for(int i=0;i<9;i++) {
+			functionsButton[i].addActionListener(this);
+			functionsButton[i].setFont(font);
+			functionsButton[i].setFocusable(false);
+		}
+		for(int i=0;i<9;i++) {
+			numbersButton[i]=new JButton(String.valueOf(i));
+			numbersButton[i].addActionListener(this);
+			numbersButton[i].setFont(font);
+			numbersButton[i].setFocusable(false);
+		}
+		negButton.setBounds(50,430,100,50);
+		delButton.setBounds(150,430,100,50);
+		clrButton.setBounds(250,430,100,50);
+		
 		
 		
 		frame.add(textfield);
